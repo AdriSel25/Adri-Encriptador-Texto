e9647436ac65e61fe9436e69d5e7ae01c741fb7b
AdriSel25 <adrisolanilla@gmail.com>
2024-03-04 12:51:38 -0500

    prueba

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const mensaje = document.querySelector(".mensaje");
 //La letra "i" es convertida para "imes"
 //La letra "a" es convertida para "ai"
 //La letra "o" es convertida para "ober"
-//La letra "u" es convertida para "ufat"
+//La letra "u" es convertida para "ufat".
 
 function btnEncriptar(){
     const textoEncriptado = encriptar(textArea.value);
